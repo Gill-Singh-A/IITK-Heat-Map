70319cb7e36be676633b6cb6937cd7da4e6049c2
Gill-Singh-A <amansg22@iitk.ac.in>
2024-05-17 06:06:29 +0530

    Added an Argument for Location

--- ccpc.py
+++ ccpc.py
@@ -24,11 +24,11 @@ def get_arguments(*args):
         parser.add_option(arg[0], arg[1], dest=arg[2], help=arg[3])
     return parser.parse_args()[0]
 
-with open("ccpc.csv", 'r') as file:
-    ccpc_ips = {line.split(',')[0]: line.split(',')[1] for line in file.read().split('\n') if line != ''}
-total_ips = len(ccpc_ips)
-ccpc_users = {ip: {"ssh_users": [], "users": []} for ip in ccpc_ips}
-ccpc_info = {ip: {"ssh_client": None, "authenticated": False, "authentication_time": None, "error": None} for ip in ccpc_ips}
+ips = None
+location = None
+total_ips = None
+location_users = None
+location_info = None
 timeout = 1
 default_users = ["root"]
 with open("users.txt", 'r') as file:
@@ -52,31 +52,42 @@ def connectSSH(ip, user, password, port=22, timeout=30):
         return err, -1
 def createPage():
     page = template_start
-    for ip, users in ccpc_users.items():
+    for ip, users in location_users.items():
         if len(users["users"]) > 0:
             status = "occupied"
         else:
             status = "free"
-        if ccpc_info[ip]["authenticated"] == False:
+        if location_info[ip]["authenticated"] == False:
             status = "power-off"
-        page += f"<tr class={status}><td>{ccpc_ips[ip]}</td><td>{(','.join(users['users'])) if len(users['users']) > 0 else '-'}</td><td>{(','.join(users['ssh_users'])) if len(users['ssh_users']) > 0 else '-'}</td><td>{status.upper()}</td></tr>\n"
+        page += f"<tr class={status}><td>{ips[ip]}</td><td>{(','.join(users['users'])) if len(users['users']) > 0 else '-'}</td><td>{(','.join(users['ssh_users'])) if len(users['ssh_users']) > 0 else '-'}</td><td>{status.upper()}</td></tr>\n"
     page += template_end
-    with open("ccpc.html", 'w') as file:
+    with open(f"pages/{location}.html", 'w') as file:
         file.write(page)
 
 if __name__ == "__main__":
     arguments = get_arguments(('-u', "--user", "user", "Computer Center (CC) User ID"),
-                              ('-t', "--timeout", "timeout", f"Timeout for Authenticating to a Linux Lab Computer (Default={timeout}seconds)"))
+                              ('-t', "--timeout", "timeout", f"Timeout for Authenticating to a Linux Lab Computer (Default={timeout}seconds)"),
+                              ('-l', "--location", "location", f"Location in Campus (Linux Lab:ccpc)"))
     if not arguments.user:
         display('-', f"Please Provide a {Back.YELLOW}CC User ID!{Back.RESET}")
         exit(0)
-    password = getpass(f"Enter Password for {arguments.user} : ")
     if not arguments.timeout:
         arguments.timeout = timeout
     else:
         arguments.timeout = int(arguments.timeout)
-    display(':', f"Linux Lab Computers = {Back.MAGENTA}{len(ccpc_ips)}{Back.RESET}")
+    if not arguments.location:
+        display('-', f"Please Provide a Location")
+        exit(0)
+    else:
+        location = arguments.location
+    password = getpass(f"Enter Password for {arguments.user} : ")
     default_users.append(arguments.user)
+    with open(f"csv/{location}.csv", 'r') as file:
+        ips = {line.split(',')[0]: line.split(',')[1] for line in file.read().split('\n') if line != ''}
+    total_ips = len(ips)
+    location_users = {ip: {"ssh_users": [], "users": []} for ip in ips}
+    location_info = {ip: {"ssh_client": None, "authenticated": False, "authentication_time": None, "error": None} for ip in ips}
+    display(':', f"IPs = {Back.MAGENTA}{total_ips}{Back.RESET}")
     try:
         ftp_server = ftplib.FTP("webhome.cc.iitk.ac.in", arguments.user, password)
     except Exception as error:
@@ -84,31 +95,31 @@ if __name__ == "__main__":
         exit(0)
     try:
         while True:
-            for ip in ccpc_ips.keys():
-                while ccpc_info[ip]["authenticated"] == False:
+            for ip in location_users.keys():
+                while location_info[ip]["authenticated"] == False:
                     ssh_client, authentication_time = connectSSH(ip, arguments.user, password, 22, timeout)
                     if authentication_time != -1:
-                        ccpc_info[ip]["authenticated"] = True
-                        ccpc_info[ip]["ssh_client"] = ssh_client
-                        ccpc_info[ip]["authentication_time"] = authentication_time
+                        location_info[ip]["authenticated"] = True
+                        location_info[ip]["ssh_client"] = ssh_client
+                        location_info[ip]["authentication_time"] = authentication_time
                     else:
-                        ccpc_info[ip]["error"] = ssh_client
+                        location_info[ip]["error"] = ssh_client
                         display('-', f"Error Occurred while Connecting to {Back.MAGENTA}{ip}{Back.RESET} => {Back.YELLOW}{ssh_client}{Back.RESET}")
                     break
-                if ccpc_info[ip]["error"] != None:
+                if location_info[ip]["error"] != None:
                     continue
-                stdin, stdout, stderr = ccpc_info[ip]["ssh_client"].exec_command("ps -aux | grep sshd")
+                stdin, stdout, stderr = location_info[ip]["ssh_client"].exec_command("ps -aux | grep sshd")
                 ssh_users = list(set([line.split(' ')[0] for line in stdout.readlines() if line.split(' ')[0] not in default_users and line.split(' ')[0] in users]))
-                stdin, stdout, stderr = ccpc_info[ip]["ssh_client"].exec_command("ps -aux | grep gnome-session")
+                stdin, stdout, stderr = location_info[ip]["ssh_client"].exec_command("ps -aux | grep gnome-session")
                 users = list(set([line.split(' ')[0] for line in stdout.readlines() if line.split(' ')[0] not in default_users and line.split(' ')[0] in users]))
                 users.sort()
-                ccpc_users[ip]["ssh_users"] = ssh_users
-                ccpc_users[ip]["users"] = users
+                location_users[ip]["ssh_users"] = ssh_users
+                location_users[ip]["users"] = users
                 display(':', f"{Back.MAGENTA}{ip}{Back.RESET} => Users:{','.join(users)}, SSH Users:{','.join(ssh_users)}")
             createPage()
             ftp_server = ftplib.FTP("webhome.cc.iitk.ac.in", arguments.user, password)
-            with open("ccpc.html", 'rb') as file:
-                ftp_server.storbinary(f"STOR /www/amansg22/www/ccpc.html", file)
+            with open(f"pages/{location}.html", 'rb') as file:
+                ftp_server.storbinary(f"STOR /www/amansg22/www/{location}.html", file)
             ftp_server.quit()
     except KeyboardInterrupt:
         display('*', f"Keyboard Interrupt Detected...", start='\n')
