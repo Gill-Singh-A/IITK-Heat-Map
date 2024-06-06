83743da5cb21a51692e22891e7bc08e9505164a0
Gill-Singh-A <amansg22@iitk.ac.in>
2024-06-07 04:01:45 +0530

    Fixed Time Format Error

--- linux_labs.py
+++ linux_labs.py
@@ -156,36 +156,39 @@ if __name__ == "__main__":
                 ssh_users = []
                 last_login_time = 1
                 for line in output:
-                    for spaces in range(20, 1, -1):
-                        line = line.replace(' '*spaces, ' '*(spaces-1))
-                    details = line.split(' ')
-                    user = details[0]
-                    if user not in default_users and user in all_cc_users:
-                        if '-' in details[2]:
-                            month = int(details[2].split('-')[1])
-                            month = list(months.keys())[month-1]
-                            login_date = int(details[2].split('-')[2])
-                            hour = int(details[3].split(':')[0])
-                            minutes = int(details[3].split(':')[1])
-                            length_for_user = len(details[4])
-                        else:
-                            month = details[2]
-                            login_date = int(details[3])
-                            hour = int(details[4].split(':')[0])
-                            minutes = int(details[4].split(':')[1])
-                            length_for_user = len(details[5])
-                        current_month = datetime.now().strftime("%B")[:3]
-                        current_date = datetime.now().day
-                        current_hour = datetime.now().hour
-                        current_minute = datetime.now().minute
-                        login_time = (months[current_month]+current_date+current_hour/24+current_minute/(24*60))-(months[month]+login_date+hour/24+minutes/(60*24))
-                        if login_time < 0.5:
-                            if length_for_user > 10:
-                                ssh_users.append(user)
-                            elif login_time < last_login_time:
-                                user_timings[user] = f"{hour}:{'0'*(2-len(str(minutes)))+str(minutes)} {login_date} {month}"
-                                last_login_time = login_time
-                                users = user
+                    try:
+                        for spaces in range(20, 1, -1):
+                            line = line.replace(' '*spaces, ' '*(spaces-1))
+                        details = line.split(' ')
+                        user = details[0]
+                        if user not in default_users and user in all_cc_users:
+                            if '-' in details[2]:
+                                month = int(details[2].split('-')[1])
+                                month = list(months.keys())[month-1]
+                                login_date = int(details[2].split('-')[2])
+                                hour = int(details[3].split(':')[0])
+                                minutes = int(details[3].split(':')[1])
+                                length_for_user = len(details[4])
+                            else:
+                                month = details[2]
+                                login_date = int(details[3])
+                                hour = int(details[4].split(':')[0])
+                                minutes = int(details[4].split(':')[1])
+                                length_for_user = len(details[5])
+                            current_month = datetime.now().strftime("%B")[:3]
+                            current_date = datetime.now().day
+                            current_hour = datetime.now().hour
+                            current_minute = datetime.now().minute
+                            login_time = (months[current_month]+current_date+current_hour/24+current_minute/(24*60))-(months[month]+login_date+hour/24+minutes/(60*24))
+                            if login_time < 0.5:
+                                if length_for_user > 10:
+                                    ssh_users.append(user)
+                                elif login_time < last_login_time:
+                                    user_timings[user] = f"{hour}:{'0'*(2-len(str(minutes)))+str(minutes)} {login_date} {month}"
+                                    last_login_time = login_time
+                                    users = user
+                    except:
+                        pass
                 location_users[ip]["ssh_users"] = ssh_users
                 location_users[ip]["users"] = users
                 display(':', f"{Back.MAGENTA}{ip}{Back.RESET} => Users:{users}, SSH Users:{','.join(ssh_users)}")
