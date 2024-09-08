40141d46eac934a28036e72265d4df691fb371e4
Gill-Singh-A <amansg22@iitk.ac.in>
2024-09-08 14:44:34 +0530

    Changed Command Line Arguments showing of Locations

--- linux_labs.py
+++ linux_labs.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-import paramiko, ftplib
+import paramiko, ftplib, os
 from getpass import getpass
 from datetime import date, datetime
 from optparse import OptionParser
@@ -104,7 +104,7 @@ if __name__ == "__main__":
     arguments = get_arguments(('-u', "--user", "user", "Computer Center (CC) User ID"),
                               ('-w', "--webhome-user", "webhome_user", "Webhome User (Default=CC User ID)"),
                               ('-t', "--timeout", "timeout", f"Timeout for Authenticating to a Linux Lab Computer (Default={timeout}seconds)"),
-                              ('-l', "--location", "location", f"Location in Campus (Linux Lab:ccpc, kd_lab)"))
+                              ('-l', "--location", "location", f"Location in Campus (Linux Lab: {', '.join([file.split('.')[0] for file in os.listdir('csv')])})"))
     if not arguments.user:
         display('-', f"Please Provide a {Back.YELLOW}CC User ID!{Back.RESET}")
         exit(0)
